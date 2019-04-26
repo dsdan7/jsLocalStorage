b9885d1b6c6b649df5b714b47e2389e44bd6e933
dsdan7 <dsdsdan7@gmail.com>
2019-04-26 11:33:37 +0900

    Project's Initial Commit

---
+++ www/components/quiz.js
@@ -0,0 +1,14 @@
+
+
+$(document).on("click","#butao", function(){
+  var ask= parseInt(localStorage.getItem("ask"));
+  if($("#ask").prop("checked")==true){
+    ask++;
+    $("#work").val("on "+ask);
+    }
+  else{
+    $("#work").val("off "+ask);
+  }
+  localStorage.setItem("ask",ask); 
+});
+
